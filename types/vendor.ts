c76fec2eb15cafe6016a10009e5a43faa30350ba
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-06-26 22:30:36 +0100

    feat: type for vendor data

---
+++ types/vendor.ts
@@ -0,0 +1,19 @@
+export type VendorRow = {
+  id: number;
+  name: string;
+  email: string;
+  phoneNumber: number;
+  businessName: string;
+  address: string;
+  socials?: string;
+  productCategory: string;
+  bankName: string;
+  accountName: string;
+  accountNumber: number;
+  storeType: string;
+  vendorID: number;
+  bankCode: number | string;
+  returnPolicy: string | number;
+  deliveryDuration: string;
+  subaccountCode: string;
+};
