596e06e264b9f16fa211818494442498fceb4ce9
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-06-26 22:31:26 +0100

    feat: wishlist zustand store with functions that will be used across components

---
+++ state-store/wishlistStore.ts
@@ -0,0 +1,66 @@
+import { WishlistProducts } from "@/types/product";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+type WishlistItem = {
+  product: WishlistProducts;
+};
+
+type WishlistStore = {
+  wishlistItems: WishlistItem[];
+  toggleWishlist: (product: WishlistProducts) => void;
+  removeFromWishlist: (productId: number) => void;
+  isInWishlist: (productId: number) => boolean;
+  clearWishlist: () => void;
+};
+
+const useWishlistStore = create<WishlistStore>()(
+  persist(
+    (set, get) => ({
+      wishlistItems: [],
+      toggleWishlist: (product: WishlistProducts) => {
+        const { wishlistItems } = get();
+
+        const exists = wishlistItems.some(
+          (item) => item.product.id === product.id,
+        );
+
+        if (exists) {
+          set({
+            wishlistItems: wishlistItems.filter(
+              (item) => item.product.id !== product.id,
+            ),
+          });
+        } else {
+          set({
+            wishlistItems: [...wishlistItems, { product }],
+          });
+        }
+      },
+
+      removeFromWishlist: (productId) => {
+        const { wishlistItems } = get();
+
+        set({
+          wishlistItems: wishlistItems.filter(
+            (item) => item.product.id !== productId,
+          ),
+        });
+      },
+
+      isInWishlist: (productId: number) => {
+        const { wishlistItems } = get();
+
+        return wishlistItems.some((item) => item.product.id === productId);
+      },
+
+      clearWishlist: () => set({ wishlistItems: [] }),
+    }),
+    {
+      name: "localmart-cart", // localStorage key
+      storage: createJSONStorage(() => localStorage),
+      // only persist the cart items, not checkout state
+      partialize: (state) => ({ wishlistItems: state.wishlistItems }),
+    },
+  ),
+);
