397f2fa18e048cf5fd3562d8e9d32b8122466144
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-06 23:08:05 +0100

    update: merge product type with a vendor type for better relationship and added a filtered product

--- state-store/productStore.ts
+++ state-store/productStore.ts
@@ -1,27 +1,36 @@
 import { create } from "zustand";
-import { ProductRow } from "@/types/product";
-
-type CartItem = {
-  product: ProductRow;
-  quantity: number;
-};
+import { ProductsWithVendor } from "@/types/product";
 
 type ProductStore = {
-  products: CartItem[];
-  editingProduct: ProductRow | null;
+  products: ProductsWithVendor[];
+  editingProduct: ProductsWithVendor | null;
   searchQuery: string;
   setSearchQuery: (query: string) => void;
-  setEditingProduct: (item: ProductRow | null) => void;
-  setProducts: (item: CartItem[]) => void;
+  setEditingProduct: (item: ProductsWithVendor | null) => void;
+  setProducts: (item: ProductsWithVendor[]) => void;
+  filteredProducts: () => ProductsWithVendor[];
 };
 
-const useProductStore = create<ProductStore>((set) => ({
+const useProductStore = create<ProductStore>((set, get) => ({
   products: [],
   searchQuery: "",
   editingProduct: null,
   setProducts: (item) => set({ products: item }),
   setEditingProduct: (item) => set({ editingProduct: item }),
   setSearchQuery: (query) => set({ searchQuery: query }),
+
+  filteredProducts: () => {
+    const { products, searchQuery } = get();
+
+    if (!searchQuery.trim()) return products;
+
+    const query = searchQuery.toLowerCase();
+    return products.filter(
+      (product) =>
+        product.item_name?.toLowerCase().includes(query) ||
+        product.category?.toLowerCase().includes(query),
+    );
+  },
 }));
 
 export default useProductStore;
