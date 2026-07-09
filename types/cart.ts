8017f22de88db26133383b73c87cac65bb6d2205
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-09 23:47:27 +0100

    feat: added a type file for cart

---
+++ types/cart.ts
@@ -0,0 +1,8 @@
+import { ProductRow, Color } from "./product";
+
+export type CartItem = {
+  product: ProductRow;
+  quantity: number;
+  selectedColor?: Color | null;
+  selectedSize?: string[] | null;
+};
