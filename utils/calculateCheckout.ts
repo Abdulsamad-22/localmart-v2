8c38604e09c5e29d6deb6c05ecbe519a8dd66afc
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-10 22:21:08 +0100

    feat: utility file for cart and checkout

---
+++ utils/calculateCheckout.ts
@@ -0,0 +1,17 @@
+import { CartItem } from "@/types/cart";
+
+export function calculateCheckout(items: CartItem[]) {
+  const subtotal = items.reduce(
+    (sum, item) => sum + item.product.item_price * item.quantity,
+    0,
+  );
+  const savings = items.reduce(
+    (sum, item) =>
+      sum + (item.product.item_price - item.product.item_price) * item.quantity,
+    0,
+  );
+  const deliveryCost = 15.99; // Free delivery over Ngn 50000
+  const tax = subtotal * 0.07; // 7% tax
+  const total = subtotal + (subtotal < 50000 ? deliveryCost : 0) + tax;
+  return { subtotal, savings, tax, total, deliveryCost };
+}
