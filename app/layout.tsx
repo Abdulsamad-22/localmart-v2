a383d817c9f15805a3d203c7c1997058178f665d
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-11 11:25:55 +0100

    update: removed header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/src/components/layout/Header";
-import Footer from "@/src/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LocalMart",
-  description: "Neighbourhood commerce for swift and low cost",
+  description: "Your Neighbourhood marketplace",
 };
 
 export default function RootLayout({
@@ -30,9 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
