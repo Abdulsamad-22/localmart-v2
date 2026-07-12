c0427deaed82474db11b85027573a62f7e26c421
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-12 22:52:17 +0100

    fix: added proper spacing to around the container

--- src/components/storeFront/StoreFront.tsx
+++ src/components/storeFront/StoreFront.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 export default function StoreFront({ limit, products }: Props) {
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 md:gap-x-4 gap-y-8 md:gap-y-12 px-6 md:px-12 my-20">
+    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 md:gap-x-4 gap-y-8 md:gap-y-12 px-6 md:px-12 my-12">
       {products.slice(0, limit).map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
