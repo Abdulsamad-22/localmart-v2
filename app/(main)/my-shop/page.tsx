f8f1bbfa58524b7db5b19981140d6856afeca28f
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-20 21:46:27 +0100

    feat: vendor's own shop page view

---
+++ app/(main)/my-shop/page.tsx
@@ -0,0 +1,36 @@
+import ShopDisplay from "@/src/components/shop/ShopDisplay";
+import MyShopHeader from "./MyShopHeader";
+import { getSupabaseServerClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Myshop() {
+  const supabase = await getSupabaseServerClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (!session) redirect("/login?redirectTo=/my-shop");
+
+  const { data: vendor } = await supabase
+    .from("vendors")
+    .select("*")
+    .eq("vendor_id", session.user.id)
+    .single();
+
+  if (!vendor) {
+    redirect("/vendor-registration");
+  }
+
+  const { data: products } = await supabase
+    .from("products")
+    .select(
+      `*, vendor:vendors(id, business_name, business_address, latitude, longitude)`,
+    )
+    .eq("vendor_id", vendor.id);
+  return (
+    <div>
+      <MyShopHeader vendor={vendor} />
+      <ShopDisplay products={products ?? []} isOwner={true} />
+    </div>
+  );
+}
