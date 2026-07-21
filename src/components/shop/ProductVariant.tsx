7db8fca1d4b47053c93c42565b9cc64211284462
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-21 10:44:28 +0100

    update: added a container to wrap size and color components

--- src/components/shop/ProductVariant.tsx
+++ src/components/shop/ProductVariant.tsx
@@ -3,10 +3,10 @@ import ProductSizesOption from "./ProductSizesOption";
 
 export default function ProductVaraiant() {
   return (
-    <>
+    <div>
       <h3 className="text-[1.125rem] mb-2">Variant</h3>
       <ProductSizesOption />
       <ProductColorOption />
-    </>
+    </div>
   );
 }
