12540a6acf34170b4e44f0382cab27dee20ad014
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-21 23:11:03 +0100

    update: renamed this file

--- src/components/storeGrid/StoreGrid.tsx
+++ src/components/storeGrid/StoreGrid.tsx
@@ -5,7 +5,7 @@ type Props = {
   products: ProductsWithVendor[];
   limit: number;
 };
-export default function StoreFront({ limit, products }: Props) {
+export default function StoreGrid({ limit, products }: Props) {
   return (
     <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 md:gap-x-4 gap-y-8 md:gap-y-12 px-6 md:px-12 my-12">
       {products.slice(0, limit).map((product) => (
