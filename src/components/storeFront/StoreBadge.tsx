8eb1d642e3f3601d97c22423ae16a2a70eff5690
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-07-24 23:39:56 +0100

    update: bg updated to use brand gradient for badge

--- src/components/storeFront/StoreBadge.tsx
+++ src/components/storeFront/StoreBadge.tsx
@@ -5,8 +5,8 @@ interface Props {
 export function StoreBadge({ children }: Props) {
   return (
     <span
-      className="inline-flex items-center rounded-full bg-[#009688]
-     px-4 py-2 text-sm font-medium text-teal-700 ring-1 ring-teal-100"
+      className=" inline-flex items-center rounded-full bg-teal-50 px-4 py-2
+       text-sm ont-medium text-teal-700 ring-1 ring-teal-100"
     >
       {children}
     </span>
