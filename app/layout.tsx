17ed851dd467e2463b18add164af216f2700b98d
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-08-04 12:57:14 +0100

    update: added vercel analtics to the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <main>
           <Toaster position="top-center" richColors />
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
