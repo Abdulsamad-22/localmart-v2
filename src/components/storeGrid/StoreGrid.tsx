76917819e3b857e463f8a0f4f6272bf2aea4b7f2
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-08-04 12:48:09 +0100

    update: updated grid for mobile screen

--- src/components/storeGrid/StoreGrid.tsx
+++ src/components/storeGrid/StoreGrid.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 export default function StoreGrid({ limit, products }: Props) {
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 md:gap-x-4 gap-y-8 md:gap-y-12 px-6 md:px-12 my-12">
+    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 md:gap-x-4 gap-y-6 md:gap-y-12 px-4 md:px-12 my-12">
       {products.slice(0, limit).map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
