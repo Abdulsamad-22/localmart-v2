0e4543a910c7066dd427efa544c969c76a8be66f
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-08-05 23:47:23 +0100

    update: removed the filter functions from the store

--- state-store/productStore.ts
+++ state-store/productStore.ts
@@ -5,32 +5,23 @@ type ProductStore = {
   products: ProductsWithVendor[];
   editingProduct: ProductsWithVendor | null;
   searchQuery: string;
+  selectedCategory: string;
   setSearchQuery: (query: string) => void;
   setEditingProduct: (item: ProductsWithVendor | null) => void;
   setProducts: (item: ProductsWithVendor[]) => void;
-  filteredProducts: () => ProductsWithVendor[];
+  setSelectedCategory: (category: string) => void;
 };
 
 const useProductStore = create<ProductStore>((set, get) => ({
   products: [],
   searchQuery: "",
   editingProduct: null,
+  selectedCategory: "",
   setProducts: (item) => set({ products: item }),
   setEditingProduct: (item) => set({ editingProduct: item }),
-  setSearchQuery: (query) => set({ searchQuery: query }),
-
-  filteredProducts: () => {
-    const { products, searchQuery } = get();
-
-    if (!searchQuery.trim()) return products;
-
-    const query = searchQuery.toLowerCase();
-    return products.filter(
-      (product) =>
-        product.item_name?.toLowerCase().includes(query) ||
-        product.item_category?.toLowerCase().includes(query),
-    );
-  },
+  setSearchQuery: (query) => set({ searchQuery: query, selectedCategory: "" }),
+  setSelectedCategory: (category) =>
+    set({ selectedCategory: category, searchQuery: "" }),
 }));
 
 export default useProductStore;
