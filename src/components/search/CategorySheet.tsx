1c5a746ce24137be027b7e9354ab8ff0916354c8
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-08-08 12:42:21 +0100

    fix: fixed category sheet list position

--- src/components/search/CategorySheet.tsx
+++ src/components/search/CategorySheet.tsx
@@ -61,47 +61,21 @@ export function CategorySheet({
       {/* Sheet */}
       <div
         className="
-          fixed bottom-0 left-0 right-0 z-50 h-[85vh] rounded-t-3xl bg-white shadow-2xl animate-in slide-in-from-bottom
-          md:left-20 md:bottom-auto md:top-1/2 md:h-[80vh] md:w-[420px] md:-translate-x-12 md:-translate-y-1/2
-          md:rounded-3xl
-        "
+    fixed bottom-0 left-0 right-0 z-50 flex h-[85vh] flex-col overflow-hidden
+    rounded-t-3xl bg-white shadow-2xl animate-in slide-in-from-bottom
+    md:bottom-auto md:left-20 lg:left-30 md:top-[30rem] lg:top-[30rem] md:h-[80vh] md:w-[420px]
+    md:-translate-x-12 md:-translate-y-1/2 md:rounded-3xl
+  "
       >
-        {/* Drag Handle */}
-
-        <div className="flex justify-center py-3">
+        <div className="flex shrink-0 justify-center py-3">
           <div className="h-1.5 w-14 rounded-full bg-gray-300" />
         </div>
 
-        {/* Header */}
-
-        <div className="flex items-center justify-between border-b border-gray-100 px-6 pb-4">
-          <div>
-            <h2 className="text-lg font-semibold text-gray-900">Categories</h2>
-
-            <p className="text-sm text-gray-500">Choose where to browse</p>
-          </div>
-
-          <button
-            onClick={onClose}
-            className="
-              flex
-              h-10
-              w-10
-              items-center
-              justify-center
-
-              rounded-full
-
-              hover:bg-gray-100
-            "
-          >
-            <X size={20} />
-          </button>
+        <div className="flex shrink-0 items-center justify-between border-b border-gray-100 px-6 pb-4">
+          {/* header */}
         </div>
 
-        {/* List */}
-
-        <div className="h-[calc(100%-84px)]">
+        <div className="min-h-0 flex-1">
           <CategoryList
             categories={categories}
             selectedCategory={selectedCategory}
