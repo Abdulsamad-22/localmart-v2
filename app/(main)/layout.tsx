e8325a95880a94ff6d715091f9983123d9383279
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-08-17 13:04:49 +0100

    fix: fetch data directly from supabase this time so refresh does bot automatically end session

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,28 @@
 import Header from "@/src/components/layout/Header";
 import Footer from "@/src/components/layout/Footer";
+import { getSupabaseServerClient } from "@/lib/supabase/server";
 
-export default function MainLayout({
+export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = await getSupabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data: vendorData } = user
+    ? await supabase
+        .from("vendors")
+        .select("id, business_name")
+        .eq("vendor_id", user.id)
+        .maybeSingle()
+    : { data: null };
   return (
     <>
-      <Header />
+      <Header user={user} vendorData={vendorData} />
       <main>{children}</main>
       <Footer />
     </>
