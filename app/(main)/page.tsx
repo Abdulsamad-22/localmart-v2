74c4d130c7d7e85938ac7dbcecb069f5183bd804
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-08-18 10:57:29 +0100

    update: product error display and refresh

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,17 @@
 import { LocationProvider } from "@/src/components/LocationProvider";
 import StoreGrid from "@/src/components/storeGrid/StoreGrid";
 import { getProducts } from "@/lib/products/getProducts";
-import { SearchBar } from "@/src/components/search/SearchBar";
 import { PageTransition } from "@/src/components/ui/PageTranstion";
+import { ProductFetchError } from "@/src/components/storeGrid/ProductFetchError";
 
 export default async function Home() {
   const result = await getProducts();
 
-  if (!result.success) return <p>Failed to load products</p>;
+  if (!result.success) return <ProductFetchError />;
 
   const products = result.data ?? [];
   return (
     <main>
-      {/* <SearchBar /> */}
       <PageTransition>
         <LocationProvider />
         <StoreGrid limit={12} products={products} />
